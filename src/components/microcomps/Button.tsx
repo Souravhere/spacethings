e8e5e5700ce330e8e5646aa13c4a6e9f969f5717
Sourav Chhimpa <souravchhimpa1@gmail.com>
2024-11-08 11:04:24 +0530

    vercel error fixed

--- src/components/microcomps/Button.tsx
+++ src/components/microcomps/Button.tsx
@@ -2,14 +2,13 @@ import React from 'react';
 
 interface ButtonProps {
   text: string;
-  onClick?: () => void;
   className?: string;
   style?: React.CSSProperties;
   icon?: React.ReactNode;
   disabled?: boolean;
 }
 
-const Button: React.FC<ButtonProps> = ({ text, onClick, className, style, icon, disabled }) => {
+const Button: React.FC<ButtonProps> = ({ text, className, style, icon, disabled }) => {
   return (
     <button
       className={`px-4 py-2 inline-block font-semibold bg-[#005ce6] rounded-lg transition ${className}`}
