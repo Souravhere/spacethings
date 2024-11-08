bb0c901f479b0ad0ba4b5f2633f7d044dc407e8a
Sourav Chhimpa <souravchhimpa1@gmail.com>
2024-11-08 23:46:06 +0530

    tech info component add to check the ui reponse

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Hersosection from "@/components/Hersosection";
+import Techinfo from "@/components/Techinfo";
 
 export default function Home() {
   return (
       <>
         <Hersosection/>
+        <Techinfo/>
         <div className="h-screen w-full"></div>
       </>
   );
