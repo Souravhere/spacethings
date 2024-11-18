7683f3650ca5c250a64fef1adacf1b8ee9410edb
Sourav Chhimpa <souravchhimpa1@gmail.com>
2024-11-18 22:09:45 +0530

    anused import removed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
-import Appdownload from "@/components/Appdownload";
-import Appshowcomp from "@/components/Appshow";
+import ALLCARE from "@/components/ALL-CARE";
+// import Appdownload from "@/components/Appdownload";
+// import Appshowcomp from "@/components/Appshow";
 import Caresection from "@/components/Caresections";
 import Hersosection from "@/components/Hersosection";
 import ServicesPage from "@/components/Servicespage";
-import Storemanagement from "@/components/Storemanagement";
+// import Storemanagement from "@/components/Storemanagement";
 // import Techinfo from "@/components/Techinfo";
-import Wassusection from "@/components/Wassu-section";
+// import Wassusection from "@/components/Wassu-section";
 
 export default function Home() {
   return (
@@ -18,6 +19,7 @@ export default function Home() {
         {/* here is two new section added from the figma file */}
 
         <Caresection/>
+        <ALLCARE/>
         {/* <Appshowcomp/>  this comp is removed due to new ui update*/}
         {/* <Storemanagement/>  this comp is removed due to new ui update*/}
         {/* <Appdownload/> this comp will update soon  */}
