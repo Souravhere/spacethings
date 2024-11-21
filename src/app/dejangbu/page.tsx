a5066ae49b00a01bc3fb38491f8a97e523fd380b
Sourav Chhimpa <souravchhimpa1@gmail.com>
2024-11-21 11:23:18 +0530

    noting

--- src/app/dejangbu/page.tsx
+++ src/app/dejangbu/page.tsx
@@ -234,7 +234,7 @@ export default function Component() {
           </Swiper>
         </div>
       </section> */}
-
+      
       {/* this is the Testimonial Section */}
       <div className='w-full bg-gradient-to-b from-blue-50 to-white py-10'>
       <h2 className="text-3xl md:text-4xl font-bold text-center">
