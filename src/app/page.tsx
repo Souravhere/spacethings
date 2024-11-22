dbfaefe87deb21156f56428d95e84edf2273b2cf
Sourav Chhimpa <souravchhimpa1@gmail.com>
2024-11-22 18:50:32 +0530

    wassu comp will be removed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,14 +7,12 @@ import Hersosection from "@/components/Hersosection";
 import ServicesPage from "@/components/Servicespage";
 import StoreOperation from "@/components/StoreOperation";
 // import Storemanagement from "@/components/Storemanagement";
-// import Wassusection from "@/components/Wassu-section";
 
 export default function Home() {
   return (
       <>
         <Hersosection/>
         <ServicesPage/>
-        {/* <Wassusection/>  this comp is removed due to new ui update*/}
         <AnalyticsDashboard/>
         <StoreOperation/>
         <Caresection/>
