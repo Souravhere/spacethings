aa807190455ac327ea6df7bbdda3af8103d2067a
Sourav Chhimpa <souravchhimpa1@gmail.com>
2024-11-30 22:46:49 +0530

    in footer both services links will be updated

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -54,7 +54,9 @@ export default function Footer() {
           <motion.div variants={itemVariants} className="space-y-4">
             <h3 className="text-lg font-semibold text-blue-600">서비스</h3>
             <ul className="space-y-2">
+              {/* replace the "/daejangbu" with the daejangbu with the website link */}
               <li><Link href="/daejangbu" className="hover:text-blue-600 transition-colors font-sans uppercase">daejangbu</Link></li>
+              {/* replace the "/watssue" with the watssue with the website link */}
               <li><Link href="/watssue" className="hover:text-blue-600 transition-colors font-sans">WATSSUE</Link></li>
             </ul>
           </motion.div>
