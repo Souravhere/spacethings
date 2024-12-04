aa5745ba91c38beaf0daca89aea749031b505bc7
Sourav Chhimpa <souravchhimpa1@gmail.com>
2024-12-04 17:05:05 +0530

    service page will now cleaned to add the our services componetn

--- src/app/service/page.tsx
+++ src/app/service/page.tsx
@@ -1,17 +1,5 @@
 "use client";
 
-import { motion } from "framer-motion";
-import { FaUserShield, FaTools, FaChartLine, FaRobot } from "react-icons/fa";
-import { MdSecurity, MdSpeed, MdMobileFriendly, MdUpdate } from "react-icons/md";
-
-
-type ServiceInfo = {
-  icon: JSX.Element;
-  title: string;
-  description: string;
-};
-
-
 export default function ServicesPage() {
   return (
     <div></div>
