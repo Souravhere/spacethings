36b154fd03530630ca9e50ff93ce790b78ee5028
Sourav Chhimpa <souravchhimpa1@gmail.com>
2024-12-04 17:04:31 +0530

    service page animation will also removed

--- src/app/service/page.tsx
+++ src/app/service/page.tsx
@@ -11,22 +11,6 @@ type ServiceInfo = {
   description: string;
 };
 
-const containerVariants = {
-  hidden: { opacity: 0 },
-  visible: {
-    opacity: 1,
-    transition: { staggerChildren: 0.2 },
-  },
-};
-
-const itemVariants = {
-  hidden: { y: 30, opacity: 0 },
-  visible: {
-    y: 0,
-    opacity: 1,
-    transition: { type: "spring", stiffness: 100 },
-  },
-}
 
 export default function ServicesPage() {
   return (
