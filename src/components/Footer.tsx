ef89938448d83538ed9d9e853ba2b4655115a17b
Sourav Chhimpa <souravchhimpa1@gmail.com>
2025-01-02 18:49:08 +0530

    i frame map will be added in the footer according to the new changes file

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -81,7 +81,18 @@ export default function Footer() {
             </ul>
           </motion.div>
         </div>
-
+        {/* Map Section */}
+        <motion.div variants={itemVariants} className="mt-12">
+          <iframe 
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2083.216744754655!2d126.97701274382433!3d37.5672632498776!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357ca28b61c565cd%3A0x858aedb4e4ea83eb!2sSeoul%2C%20South%20Korea!5e0!3m2!1sen!2sin!4v1735801371017!5m2!1sen!2sin" 
+            width="100%" 
+            height="450" 
+            style={{ border: 0 }} 
+            allowFullScreen={true} 
+            loading="lazy" 
+            referrerPolicy="no-referrer-when-downgrade"
+          ></iframe>
+        </motion.div>
         {/* Bottom Bar */}
         <motion.div 
           variants={itemVariants}
